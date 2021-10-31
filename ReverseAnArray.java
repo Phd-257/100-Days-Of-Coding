5108b6e5c5ea7abdd382a01774f3a45739f7e4af
Harsh Patel <63013932+Phd-257@users.noreply.github.com>
2021-10-30 22:15:38 -0400

    Day 2
    GeeksForGeeks easy problem

---
+++ ReverseAnArray.java
@@ -0,0 +1,39 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class ReverseAnArray {
+
+    public static void main(String[] args) {
+        
+        Scanner sc= new Scanner(System.in);
+        
+        
+        int testCases = sc.nextInt();
+
+        while(testCases>0){
+       
+        int sizeOfAnArray = sc.nextInt();
+        
+        int[] arr= new int[sizeOfAnArray];
+        for (int i = 0; i < sizeOfAnArray; i++) {
+
+            
+            arr[i]= sc.nextInt();
+            
+        }
+        
+        
+
+        for (int i = arr.length-1; i >=0 ; i--) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println("");
+        testCases--;
+ 
+    }
+
+
+    }
+    
+}
