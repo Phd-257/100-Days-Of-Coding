f7c727f9f5e142f6decc3a507d3a0997e5378d96
Harsh Patel <63013932+Phd-257@users.noreply.github.com>
2021-11-06 17:32:29 -0400

    DAY 9 Insertion Sort

---
+++ InsertionSort.java
@@ -0,0 +1,24 @@
+package Hundred_Days_Of_Code;
+
+public class InsertionSort {
+    
+    public static void main(String[] args) {
+        int[] a = {50,10,40,30,20};
+        for (int i = 1; i < a.length; i++) {
+            int key = a[i];
+            int j=i-1;
+
+            while(j>=0 &&a[j]>key  ){
+                 a[j+1]=a[j];
+                 j--;
+             }
+             a[j+1]=key;
+
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]+" ");
+        }
+        
+    }
+}
