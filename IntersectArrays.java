903cf8fcb40249ea7b378d73a643939729ad15a6
Harsh Patel <63013932+Phd-257@users.noreply.github.com>
2021-11-17 01:15:13 -0500

    Day 19 LeetCode
    Forget to push

---
+++ IntersectArrays.java
@@ -0,0 +1,44 @@
+package Hundred_Days_Of_Code;
+
+import java.util.Arrays;
+
+public class IntersectArrays {
+
+    public static void main(String[] args) {
+        
+        int[] nums1  = {1,3,2,2};
+        int[] nums2= {2,2};
+
+        String st="";
+
+        int k=0;
+
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+
+                if(nums1[i]==nums2[j]){
+                        //n[k++] = nums1[i];
+                        st=st+String.valueOf(nums1[i]);
+                        break;
+                }
+                
+                
+            }
+        }  
+        
+        String[] newk = st.split("");
+        int[] n  = new int[st.length()];
+
+        
+        for (int i = 0; i < st.length(); i++) {
+            n[i]=Integer.valueOf(newk[i]);
+            
+            
+        }
+
+        for (int i : n) {
+            System.out.print(i+" ");
+        }
+    }
+ 
+}
