417982bb098a9cdb6810e45bf3d15a54430b28f1
Harsh Patel <63013932+Phd-257@users.noreply.github.com>
2021-12-26 23:23:44 -0500

    Day 35 Merge two sorted Array
    LeetCode easy

--- MeargedSortedArray.java
+++ MeargedSortedArray.java
@@ -2,31 +2,34 @@ public class MeargedSortedArray {
     
     public static void main(String[] args) {
         
-        int nums1[] = {1,2,3,0,0,0}, m = 3, nums2[] = {2,5,6};
+        int nums1[] = {2,3,8,0,0,0}, m = 3, nums2[] = {1,5,6};
 
         merge(nums1, m, nums2, m);
+        for (int i : nums1) {
+            System.out.print(i+" ");
+        }
     }
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i=0;
-        int j=0;
-        int k=0;
-        int x = nums1.length-1-m;
-        int y=nums2.length-1;
-        while(i<x && j<y){
-
-            if(nums1[i]<nums2[j]){
-                nums1[k]= nums1[i];
-                i++;
-                k++;
-
-            }
-            else{
-                int temp = nums2[j];
-                nums2[j]=nums1[i];
-                nums1[i]=temp;
-                
-            }
-
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int[] ans = new int[n+m];
+        while(i < m && j < n){
+            if(nums1[i] <= nums2[j]){
+            ans[k++] = nums1[i++];
+        }
+        else{
+             ans[k++] = nums2[j++];
+        }
+        }
+        while(i < m){
+            ans[k++] = nums1[i++];
+        }
+        while(j < n){
+            ans[k++] = nums2[j++];
+        }
+        for(int p=0;p<n+m;p++){
+            nums1[p] = ans[p];
         }
     }
 }
